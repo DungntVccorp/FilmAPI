7ee43ca923df3d0bb65358f2c3bbecb13c1fbe9f
DungntVccorp <dung.nt.a5901679@gmail.com>
2015-05-18 11:48:41 +0700

    1.0.0

---
+++ Pod/Classes/FilmAPI/DzFilmAPI.h
@@ -0,0 +1,19 @@
+//
+//  DzFilmAPI.h
+//  FilmAPI
+//
+//  Created by Nguyen Tien Dung on 5/18/15.
+//  Copyright (c) 2015 DungntVccorp. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "M3U8.h"
+#import "PlayerItem.h"
+#import "FilmChapter.h"
+#import "FilmAPI.h"
+#import "FilmModel.h"
+#import "NSString+DecodeString.h"
+#import "Subtitle.h"
+@interface DzFilmAPI : NSObject
+
+@end
---
+++ Pod/Classes/FilmAPI/DzFilmAPI.m
@@ -0,0 +1,12 @@
+//
+//  DzFilmAPI.m
+//  FilmAPI
+//
+//  Created by Nguyen Tien Dung on 5/18/15.
+//  Copyright (c) 2015 DungntVccorp. All rights reserved.
+//
+
+#import "DzFilmAPI.h"
+@implementation DzFilmAPI
+
+@end

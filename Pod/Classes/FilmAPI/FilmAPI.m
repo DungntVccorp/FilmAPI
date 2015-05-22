6cbd82cde8dbba75c310e70a80b7f5f720a88d65
DungntVccorp <dung.nt.a5901679@gmail.com>
2015-05-22 16:30:36 +0700

    fix filmName
    fix filmName

--- Pod/Classes/FilmAPI/FilmAPI.m
+++ Pod/Classes/FilmAPI/FilmAPI.m
@@ -317,7 +317,7 @@
             f.filmID = [[[[f.filmURL componentsSeparatedByString:@"."] firstObject] componentsSeparatedByString:@"-"] lastObject];
             if(arr.count == 1){
                 TFHppleElement *e = arr.lastObject;
-                f.FilmName = [e.text stringByStrippingWhitespace];
+                f.FilmName = [e.raw stringByStrippingWhitespace];
                 
             }
             
